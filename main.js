cf7e6ab9376270a72e19f290d580cc981d65a47e
vj9898 <81862570+vj9898@users.noreply.github.com>
2021-04-25 16:04:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var images =[
+    "91zbi9M+mKL.jpg",
+    "download.png",
+    "mother-drawing-child-clip-art-png-favpng-q8cDP2Q3QZR622b4Tc1gShhFn.jpg",
+    "f0caf9ad93bfea30a46faa27e6ac56ba.jpg",
+    "images.png",
+  ];                           
+  var i=1; function nextimage() { document.getElementById("people").src = images[i]; i++; }
+  if(i==5){
+    i=1;
+  }
